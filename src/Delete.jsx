664e42b7e59f1f496f817172cde104b69e0fb146
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2025-07-21 21:53:22 +0300

    Modified delete

--- src/Delete.jsx
+++ src/Delete.jsx
@@ -4,7 +4,7 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 const Delete = (props) => {
   return (
     <>
-      <button onClick={() => {props.delete(props.id)}} className="text-red-600 -400 text-4xl shadow-lg rounded-full p-2 hover:bg-gray-200 cursor-pointer">
+      <button onClick={() => {propsdelete(props.id)}} className="text-red-600 -400 text-4xl shadow-lg rounded-full p-2 hover:bg-gray-200 cursor-pointer">
         <RiDeleteBin6Line />
       </button>
     </>
